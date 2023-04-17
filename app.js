3093bfecb7e9efe8cbc16daf31a00e3e12d942ce
Abhishek Patel <98528020+Abhi8755@users.noreply.github.com>
2023-04-17 12:45:05 +0530

    dark mode and light mide added

--- app.js
+++ app.js
@@ -5,3 +5,33 @@ const link = document.getElementById('link')
 link.classList.toggle('navLinks')
 
 })
+
+
+
+
+let darkBtn = document.getElementById('darkBtn')
+let lightBtn = document.getElementById('lightBtn')
+
+function changecolor (){
+    document.body.style.backgroundColor = 'black'
+    let text = document.querySelectorAll('.text')
+    for(i=0; i<text.length; i++){
+        text[i].style.color = 'white'
+    }
+   lightBtn.style.display = 'block'
+   darkBtn.style.display = 'none'
+}
+function lightcolor (){
+    document.body.style.backgroundColor = '#f0f4f5'
+    let text = document.querySelectorAll('.text')
+    for(i=0; i<text.length; i++){
+        text[i].style.color = 'black'
+    }
+   lightBtn.style.display = 'none'
+   darkBtn.style.display = 'block'
+}
+
+
+
+darkBtn.addEventListener('click', changecolor)
+lightBtn.addEventListener('click', lightcolor)
